08e31f4c9d41443a4db63f8ead6f4b0217551f25
Tk10 <60160284@go.buu.ac.th>
2021-04-10 22:25:45 +0700

    Hello Saturday

---
+++ cssComponent/8-filter/js/filter.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  var x = document.getElementById(".container");
+  if (x.style.display === "none") { 
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}

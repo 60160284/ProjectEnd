6488106ed9d6050ba21fcff18738033e5cfb663d
Tk10 <60160284@go.buu.ac.th>
2021-04-12 16:47:06 +0700

    Hello Dev Monday

--- cssComponent/8-filter/js/filter.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction() {
-  var x = document.getElementById(".container");
-  if (x.style.display === "none") { 
-    x.style.display = "flex";
-  } else {
-    x.style.display = "none";
-  }
-}
